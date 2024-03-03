ddbc7bae011a5722fc151ea645ded524aa95546b
artttt_tttt <tanapath.tp@gmail.com>
2024-03-03 23:44:58 +0700

    commit

---
+++ client-ts/src/components/content.tsx
@@ -0,0 +1,14 @@
+function Content() {
+    return (
+        <div className="content">
+            <div className="content-about-grid">
+                <div className="footer-col">
+                    <h5>Cloud Project</h5>
+                    <a>Web Demo for Cloud Project</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Content;
